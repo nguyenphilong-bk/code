89057f1147e1cf367164f7b4249480a6f041e52b
long <long.np@geekup.vn>
2021-08-02 11:04:32 +0700

    daily

---
+++ Tree/ID_111.cpp
@@ -0,0 +1,56 @@
+#include <queue>
+#include <iostream>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int minDepth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+
+        std::queue<TreeNode *> aQueue;
+        int result = 1;
+
+        aQueue.push(root);
+
+        while (!aQueue.empty())
+        {
+            int size = aQueue.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *temp = aQueue.front();
+                if (!temp->left && !temp->right)
+                    return result;
+                if (temp->left)
+                    aQueue.push(temp->left);
+
+                if (temp->right)
+                    aQueue.push(temp->right);
+
+                aQueue.pop();
+            }
+            result++;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution aSolution;
+    TreeNode *root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+    int result = aSolution.minDepth(root);
+    std::cout << result << std::endl;
+}
