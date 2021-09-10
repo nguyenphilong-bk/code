be2a970785bfe2a5e2c2faf261c03fc80ae41251
long <long.np@geekup.vn>
2021-09-10 20:10:46 +0700

    daily

---
+++ BinarySearchSolo/LinkedListToBinarySearchTree.js
@@ -0,0 +1,28 @@
+/**
+ * class Tree {
+ *   constructor(val, left=null, right=null) {
+ *     this.val = val
+ *     this.left = left
+ *     this.right = right
+ *   }
+ * }
+ */
+
+/**
+ * class LLNode {
+ *   constructor(val, next=null) {
+ *     this.val = val
+ *     this.next = next
+ *   }
+ * }
+ */
+class Solution {
+    solve(node) {
+        let curr = node;
+        let size = 0;
+        while (curr) {
+            size++;
+            curr = curr.next;
+        }
+    }
+}
