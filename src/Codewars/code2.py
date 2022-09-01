74e44844a181864556a1939c4f38bf68311339e6
long.np <nguyenphilong.bkevent@gmail.com>
2022-09-01 22:27:36 +0700

    daily coding

--- src/Codewars/code2.py
+++ src/Codewars/code2.py
@@ -2,4 +2,4 @@ from functools import reduce
 
 
 def high(x: str):
-    return max(x.split(' '), key=lambda ele: sum(ord(c) - 96 for c in "abc"))
+    return max(x.split(' '), key=lambda ele: sum(ord(c) - 96 for c in ele))
